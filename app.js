18ce059d2973274fe2b615f32b5f8d4e6d963caf
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-10-29 11:42:04 -0300

    Uses json to output API info

---
+++ app.js
@@ -0,0 +1,6 @@
+var json = {"id":2,"description":"Apartamento Mobiliado, Ótima Localização","code":"np3202","main_picture":"http://www.kevinandamanda.com/whatsnew/wp-content/uploads/2012/10/new-house-1.jpg","address":{"id":7,"cep":"59.151-250","street":"Avenida Abel Cabral","neighborhood":"Nova Parnamirim","city":"Parnamirim","state":"RN","number":"1914","complement":"","area":"Zona Sul","condominium_id":null,"created_at":"2014-10-29T00:09:17.700Z","updated_at":"2014-10-29T00:09:17.700Z"},"url":"http://potato-machine-111353.sae1.nitrousbox.com/real_states/2.json?locale=pt-BR"};
+
+angular.module('app', [])
+.controller('jubileus', function($scope) {
+    $scope.json = json; 
+});
