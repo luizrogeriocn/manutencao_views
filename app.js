f561e92dd754b4b0453da8a3706f71e2397c9114
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-10-29 15:56:27 -0300

    Adds new layout and json

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
-var json = {"id":2,"description":"Apartamento Mobiliado, Ótima Localização","code":"np3202","mainPicture":"http://www.kevinandamanda.com/whatsnew/wp-content/uploads/2012/10/new-house-1.jpg","address":{"id":7,"cep":"59.151-250","street":"Avenida Abel Cabral","neighborhood":"Nova Parnamirim","city":"Parnamirim","state":"RN","number":"1914","complement":"","area":"Zona Sul","condominium_id":null,"created_at":"2014-10-29T00:09:17.700Z","updated_at":"2014-10-29T00:09:17.700Z"},"url":"http://potato-machine-111353.sae1.nitrousbox.com/real_states/2.json?locale=pt-BR"};
+var json = [
+	{
+		id: 1,
+		description: "Casa em ponta negra",
+		code: "A123",
+		main_picture: "http://youveneverheardofjentidwell.files.wordpress.com/2012/03/house.jpg",
+		address: {
+			id: 3,
+			cep: "59.040-226",
+			street: "Rua dos bobos",
+			neighborhood: "Bairro dos Bobos",
+			city: "Natal",
+			state: "RN",
+			number: "0",
+			complement: null,
+			area: "Zona Sul",
+			condominium_id: null,
+			created_at: "2014-10-29T17:42:25.550Z",
+			updated_at: "2014-10-29T17:42:25.550Z"
+		},
+		url: "http://localhost:3000/real_states/1.json?locale=pt-BR"
+	},
+	{
+		id: 2,
+		description: "Casa no Alecrim",
+		code: "AACC2",
+		main_picture: "http://cdn.freshome.com/wp-content/uploads/2013/04/Modern-A-House-17.jpg",
+		address: {
+			id: 4,
+			cep: "59.040-226",
+			street: "Rua dos Javas",
+			neighborhood: "Bairro dos Bobos",
+			city: "Natal",
+			state: "RN",
+			number: "0",
+			complement: null,
+			area: "Zona Leste",
+			condominium_id: null,
+			created_at: "2014-10-29T17:42:25.566Z",
+			updated_at: "2014-10-29T17:42:25.566Z"
+		},
+		url: "http://localhost:3000/real_states/2.json?locale=pt-BR"
+	},
+	{
+		id: 3,
+		description: "Casa no Limbo",
+		code: "LACC2",
+		main_picture: "http://www.e-architect.co.uk/images/jpgs/sydney/northbridge-house-r210813-m2.jpg",
+		address: {
+			id: 5,
+			cep: "59.040-226",
+			street: "Rua dos Limbos",
+			neighborhood: "Bairro dos Bobos",
+			city: "Natal",
+			state: "RN",
+			number: "0",
+			complement: null,
+			area: "Zona Norte",
+			condominium_id: null,
+			created_at: "2014-10-29T17:42:25.572Z",
+			updated_at: "2014-10-29T17:42:25.572Z"
+		},
+		url: "http://localhost:3000/real_states/3.json?locale=pt-BR"
+	}
+];
 
 angular.module('app', [])
 .controller('jubileus', function($scope) {
